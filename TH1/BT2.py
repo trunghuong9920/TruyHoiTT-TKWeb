83f87f625cce1e9e3f2878d28385f3fcb20e63cd
trunghuong9920 <trunghuongcdbk@gmail.com>
2022-02-28 20:46:41 +0700

    update

--- TH1/BT2.py
+++ TH1/BT2.py
@@ -23,7 +23,7 @@ porter = nltk.PorterStemmer()
 lists2 = [porter.stem(t) for t in tokens2]
 
 # ----------------
-url3 = 'https://www.gutenberg.org/files/67493/67493-h/67493-h.htm'
+url3 = 'https://www.gutenberg.org/files/67511/67511-h/67511-h.htm'
 html3 = request.urlopen(url3).read().decode('utf8')
 raw3 = BeautifulSoup(html3, 'html.parser').get_text()
 tokens3 = nltk.word_tokenize(raw3)
@@ -32,16 +32,17 @@ porter = nltk.PorterStemmer()
 
 lists3 = [porter.stem(t) for t in tokens3]
 
-listText = [lists, lists2,lists3]
+listText = [lists[:10], lists2[:10],lists3[:10]]
+dic = dict()
 
-dic = {}
-count = 0
-for i in listText:
-    count += 1
-    for x in i:
-        if x in dic:
-            dic[x] += 1
-            dic[count] += 1
-        else:
-            dic[x] = 1
-print(dic)
+for index in range(len(listText)):
+    for doc in listText:
+        for word in doc:
+            if word in dic:
+                if index in dic[word]:
+                    dic[word][index] += 1
+                else:
+                    dic[word][index] = 1
+            else:
+                dic[word] = dict()
+                dic[word][index] = 1
